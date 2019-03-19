28c3731df38e62e5a6ceda653d57b5f0ea354482
Sebastian Springer <info@sebastian-springer.com>
2019-03-19 13:48:15 +0100

    async

--- spec/async.spec.js
+++ spec/async.spec.js
@@ -1,13 +1,36 @@
-describe('myAsyncFunction', () => {
-  it('should return 42', done => {
-    // arrange
-    const cb = function(value) {
+describe('async', () => {
+  describe('myAsyncFunction', () => {
+    it('should return 42', done => {
+      // arrange
+      const cb = function(value) {
+        // assert
+        expect(value).toBe(42);
+        done();
+      };
+
+      // act
+      myAsyncFunction(cb);
+    });
+  });
+
+  describe('myAsyncFunction2', () => {
+    it('should return 42', async () => {
+      // arrange
+      // act
+      const value = await myAsyncFunction2();
       // assert
       expect(value).toBe(42);
-      done();
-    };
+    });
+  });
 
-    // act
-    myAsyncFunction(cb);
+  it('should work', done => {
+    myAsyncFunction2().then(value => {
+      expect(value).toBe(42);
+      done();
+    });
+  });
+
+  it('should work', () => {
+    return myAsyncFunction2().then(value => expect(value).toBe(42));
   });
 });
